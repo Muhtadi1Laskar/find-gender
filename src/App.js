ef467414c01be9609548c009dcc0ca1093f54223
Muhtadi <laskar90muhtadi@gmail.com>
2020-05-20 09:14:30 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Image, Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState();
+  const [showData, setShowData] = useState(false);
+
+  const apiCall = async () => {
+    const name = document.getElementById("gender-input").value;
+    if(name) {
+      const res = await fetch(
+        `https://gender-api.com/get?name=${name}&key=whUrNZLgHuKVWlWtZw`
+      );
+      res
+        .json()
+        .then((res) => {
+          console.log(res);
+          setData(res);
+          setShowData(true);
+        })
+        .then((err) => {
+          return err;
+        } );
+    }
+    else {
+      alert("Enter a valid name");
+    }
+    
+  };
+
+  const renderTags = () => {
+    if (data.gender === "male") {
+      return (
+        <div className="card">
+          <Image
+            src={"https://image.flaticon.com/icons/png/512/123/123172.png"}
+          />
+          <div className="text">
+            <p>Gender : {data.gender} </p>
+            <p>Name : {data.name_sanitized}</p>
+            <p>Accuracy : {data.accuracy}</p>
+          </div>
+        </div>
+      );
+    } else if (data.gender === "female") {
+      return (
+        <div className="card">
+          <Image
+            src={"https://image.flaticon.com/icons/png/512/123/123164.png"}
+          />
+          <div className="text">
+            <p>Gender : {data.gender} </p>
+            <p>Name : {data.name_sanitized}</p>
+            <p>Accuracy : {data.accuracy}</p>
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="container-body">
+      <Row>
+        <h1>Check Gender</h1>
+      </Row>
+      <Row>
+        <input
+          name="input"
+          id="gender-input"
+          className="input"
+          placeholder="Enter your name"
+        />
+      </Row>
+      <Row>
+        <button className="btn" onClick={apiCall}>
+          Submit
+        </button>
+      </Row>
+      <Row>{showData === true && renderTags()}</Row>
+    </Container>
   );
 }
 
